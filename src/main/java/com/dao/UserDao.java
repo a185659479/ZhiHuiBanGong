bf116907dd83ee0ab96816d7720226cd0a99cd42
a185659479 <185659479@qq.com>
2019-03-29 16:08:17 +0800

    ZhiHuiBanGong0329

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -1,8 +1,6 @@
 package com.dao;
 
-public interface UserDao<T> {
-    public void addUser(T entity);
-    public void editUser(T entity);
-    public void dscontinue_use(Long id);
+public interface UserDao {
+
 
 }
---
+++ src/main/java/com/service/impl/UserDaoImpl.java
@@ -0,0 +1,7 @@
+package com.service.impl;
+
+import com.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+
+}
